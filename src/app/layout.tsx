567a2bc377845cc36b107f7c66c18fbf37004ee1
jeetm31 <jeetmajumder858@gmail.com>
2024-06-18 16:45:06 +0530

    title change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ import ThemeSwitch from "@/components/panel/ThemeSwitch";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "urFolio - Portfolio web",
-  description: "urFolio - a Open Source portfolio web application",
+  title: "Jeet Majumder",
+  description: "Jeet Majumder's portfolio website",
 };
 
 export default function RootLayout({
